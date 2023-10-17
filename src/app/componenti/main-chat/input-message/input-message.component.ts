867435fd3c546bb833708f28dff93af6136649d0
Jero1304 <calogeroleto13@gmail.com>
2023-10-17 14:54:25 +0200

    arriva il messaggio

--- src/app/componenti/main-chat/input-message/input-message.component.ts
+++ src/app/componenti/main-chat/input-message/input-message.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-input-message',
   templateUrl: './input-message.component.html',
-  styleUrls: ['./input-message.component.css']
+  styleUrls: ['./input-message.component.css'],
 })
 export class InputMessageComponent {
-
+  constructor() {}
+  onSubmit(message: NgForm) {
+    console.log(message.value);
+  }
 }
