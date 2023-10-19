fc46f7c96710d5242d221a986fe3c9ed9d568a17
Jero1304 <calogeroleto13@gmail.com>
2023-10-19 09:05:10 +0200

    fix contact

--- src/app/componenti/main-chat/contact-chat/contact-chat.component.ts
+++ src/app/componenti/main-chat/contact-chat/contact-chat.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild, AfterViewInit, OnDestroy
+} from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { ContactService } from 'src/app/service/contact.service';
 
 @Component({
   selector: 'app-contact-chat',
   templateUrl: './contact-chat.component.html',
-  styleUrls: ['./contact-chat.component.css']
+  styleUrls: ['./contact-chat.component.css'],
 })
-export class ContactChatComponent {
-  constructor(private contactService: ContactService) {}
+export class ContactChatComponent implements AfterViewInit {
+  @ViewChild('lastMessageContainer', { read: ElementRef })
+  lastMessageContainer!: ElementRef;
+
+
+  constructor(
+    private contactService: ContactService,
+  ) {}
+
+  ngAfterViewInit(): void {
+  }
+
 
   get currentChat() {
     // console.log(this.contactService.getCurrentContact());
