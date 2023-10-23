ec11e8d53fbcc59a96c8995f2aa9be04c9955f13
Jero1304 <calogeroleto13@gmail.com>
2023-10-23 12:12:24 +0200

    FIX date

--- src/app/componenti/main-chat/contact-chat/contact-chat.component.ts
+++ src/app/componenti/main-chat/contact-chat/contact-chat.component.ts
@@ -4,9 +4,12 @@ import {
   ViewChild,
   AfterViewChecked,
 } from '@angular/core';
+import { format } from 'date-fns';
 import { FormGroup } from '@angular/forms';
 import { ContactService } from 'src/app/service/contact.service';
-
+interface GroupedMessages {
+  [date: string]: any[];
+}
 @Component({
   selector: 'app-contact-chat',
   templateUrl: './contact-chat.component.html',
@@ -36,4 +39,33 @@ export class ContactChatComponent implements AfterViewChecked {
   get currentChat() {
     return this.contactService.getCurrentContact();
   }
+
+  formattaOraMessaggio(dataString: string): string {
+    console.log('dataString',dataString);
+    const messageDate = new Date(dataString);
+    console.log('messageDate',messageDate);
+    // console.log('format',format(messageDate, 'MM/dd/yyyy'));
+    return format(messageDate, 'dd/MM/yyyy');
+    // return format(messageDate, 'MM/dd/yyyy');
+  }
+
+  groupMessagesByDay(messages: any[]): GroupedMessages {
+    const groupedMessages: GroupedMessages = {};
+    for (const message of messages) {
+      const messageDate = new Date(message.date);
+  
+      // Verifica se la data è valida prima di procedere
+      if (!isNaN(messageDate.getTime())) {
+        const dayKey = this.formattaOraMessaggio(message.date);
+        // const dayKey = format(messageDate, 'MM/dd/yyyy');
+  
+        if (!groupedMessages[dayKey]) {
+          groupedMessages[dayKey] = [];
+        }
+        groupedMessages[dayKey].push(message);
+      }
+    }
+    return groupedMessages;
+  }
+  
 }
